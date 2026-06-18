78df7d749380960e00b5dd02d5c7faa61663e015
moshe <hyim695064@gmail.com>
2026-06-18 17:32:10 +0300

    secion7

---
+++ mission_control.py
@@ -0,0 +1,18 @@
+agent_name = "bond"
+mission_cod = 2134
+distance_to_target = 2.5 
+mission_active_status = (True)
+print(agent_name)
+print(mission_cod,distance_to_target,mission_active_status)
+print(type(agent_name))
+print(type(mission_cod))
+print(type(distance_to_target))
+print(type(mission_active_status))
+travel_distance_feature = (distance_to_target*2)
+print(travel_distance_feature)
+fual_usage = 1
+full_estimation = fual_usage*2
+print(full_estimation)
+total_fual = 10
+fual_after_mission = total_fual-full_estimation
+print(fual_after_mission)
