118156b7f3f6a273658369271f11c3e3d2356d83
moshe <hyim695064@gmail.com>
2026-06-18 18:18:57 +0300

    secion8

--- mission_control.py
+++ mission_control.py
@@ -16,3 +16,11 @@ print(full_estimation)
 total_fual = 10
 fual_after_mission = total_fual-full_estimation
 print(fual_after_mission)
+countdown_feature = input("how long until your mission will be done in seconds")
+countdown_feature = int(countdown_feature)
+print(countdown_feature)
+countdown_minutes = countdown_feature / 60
+coundown_hours = countdown_minutes / 60
+print("seconds", countdown_feature)
+print("minutes", countdown_minutes)
+print("hours", coundown_hours)
